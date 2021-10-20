80b4681265d9b2f75fdb2d61a4b4365fb03f2b95
OGRoomService <51579862+OGRoomService@users.noreply.github.com>
2021-10-20 13:39:45 -0400

    404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import HomePage from './pages/HomePage';
 import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 export default function App() {
   return (
-    <switch>
-      <Route exact path="/" component={HomePage}/>
-      <Route exact path="/main" component={MainPage}/>
-      <Route exact path="/login" component={LoginPage}/>
-      <Route path="/user/:id" component={UserPage}/>
-      <Route path="*">
-        <h1>404 Not Found</h1>
-      </Route>
-    </switch>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/main" component={MainPage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route path="/user/:id" component={UserPage} />
+      <Route component={NotFound} />
+    </Switch>
   )
 }
