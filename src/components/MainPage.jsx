eb1d0f98ab5da646405793857ea55a8b78e22223
CharlesStanski <90924992+CharlesStanski@users.noreply.github.com>
2021-10-25 10:52:45 -0400

    Styled main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import "../styles/main.css";
 
 export default function MainPage() {
     return (
@@ -9,9 +10,6 @@ export default function MainPage() {
             <div className="container" id="container-t">
                 <div className="container">
                     <div className="container" id="container-l">
-                        <ul>
-                            <li><a href="#head">Post Feed</a></li>
-                        </ul>
                         <h1>Spacing</h1>
                         <div
                         //className="grid-item" id="box-one"
@@ -24,17 +22,9 @@ export default function MainPage() {
                         <input type="text" placeholder="Search.." />
                         <div className="head"></div>
                         <h1>Post Feed</h1>
-                        <div className="LikeButton"></div>
-                        <div id="like_button_container"></div>
-                        <script src="like_button.tsx"></script>
-                        <div className="grid-item" id="box-two">
-                        </div>
                     </div>
                     <div className="container" id="container-r">
-                        <ul>
-                            <li><a href="#user">Username</a></li>
-                            <li><a href="#profile">Profile</a></li>
-                        </ul>
+                        
                         <h1>Friends List</h1>
                     </div>
                 </div>
