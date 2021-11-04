595c2e40713b1ef5cb3ff317579fd56c88ea99da
OGRoomService <51579862+OGRoomService@users.noreply.github.com>
2021-11-04 19:20:20 -0400

    Component rewrite, multi-step register form
    - class components are no longer recommended, working on removing them
    - working on building a multi-step registration form

--- src/components/Registration/RegistrationUserDetails.js
+++ src/components/Registration/RegistrationUserDetails.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 const RegistrationUserDetails = ({ nextStep, handleChange, values }) => {
+    const [data, setData] = useState(null);
+
     const Continue = e => {
         e.preventDefault();
+        /* setData({
+            email:'test'
+        }); */
+        /* console.log(data); */
         nextStep();
     }
 
@@ -14,42 +21,42 @@ const RegistrationUserDetails = ({ nextStep, handleChange, values }) => {
                 <p>
                     <label htmlFor="uname">Username</label>
                     <input
-                        id="uname"
-                        name="uname"
+                        id="username"
+                        name="username"
                         type="text"
                         placeholder="username"
                         defaultValue={values.username}
-                        onChange={handleChange('username')} />
+                        onChange={handleChange} />
                 </p>
                 <p>
                     <label htmlFor="uemail">Email</label>
                     <input
-                        id="uemail"
-                        name="uemail"
+                        id="email"
+                        name="email"
                         type="email"
                         placeholder="email"
                         defaultValue={values.email}
-                        onChange={handleChange('email')} />
+                        onChange={handleChange} />
                 </p>
                 <p>
                     <label htmlFor="pass">Password</label>
                     <input
-                        id="pass"
-                        name="pass"
+                        id="password"
+                        name="password"
                         type="password"
                         placeholder="Password"
                         defaultValue={values.password}
-                        onChange={handleChange('password')} />
+                        onChange={handleChange} />
                 </p>
                 <p>
                     <label htmlFor="vpass">Confirm password</label>
                     <input
-                        id="vpass"
-                        name="vpass"
+                        id="vpassword"
+                        name="vpassword"
                         type="password"
                         placeholder="Confirm Password"
                         defaultValue={values.vpassword}
-                        onChange={handleChange('vpassword')} />
+                        onChange={handleChange} />
                 </p>
                 <input type="submit" value="Sign Up" />
             </form>
