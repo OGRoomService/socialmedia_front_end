6c6e7e5bc81e18036d80d845f42a0c3539115a40
Tukhin <77026893+Tukhin@users.noreply.github.com>
2021-11-07 01:15:32 -0400

    adjusting app.js

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -26,7 +26,7 @@ function LoginFormFunction() {
     return (
         <>
             <div>
-            <h2>Rowanspace</h2>
+            <h2>Rowanspace11111</h2>
                 <div className="containerL">
                     <div className="containerL" id="containerL-m">
                         <h1>Login</h1>
@@ -51,7 +51,7 @@ function LoginFormFunction() {
                                 id="login-password"
                                 name="pass"
                                 type="text"
-                                placeholder="Password"
+                                placeholder="Password1111"
                                 onChange={handleUpdate}
                                 required />
                             <input type="submit" value="Login"/>
