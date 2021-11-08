75f0734c71c05555b445aa35a1c66b7047a47862
CharlesStanski <90924992+CharlesStanski@users.noreply.github.com>
2021-11-08 10:48:31 -0500

    App.js with profile page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router';
 import UserPage from './components/UserPage';
 import LoginPage from './components/LoginPage';
 import MainPage from './components/MainPage';
+import ProfilePage from './components/ProfilePage';
 import NotFound from './components/NotFound';
 import { Registration } from './components/Registration/Registration';
 import './App.css';
@@ -17,6 +18,7 @@ export default function App() {
       <Route exact path="/home" component={MainPage} />
       <Route exact path="/register" component={Registration} />
       <Route path="/u/:id" component={UserPage} />
+      <Route exact path="/profile" component={ProfilePage} />
       <Route component={NotFound} />
     </Switch>
   )
