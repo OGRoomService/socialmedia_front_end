8feb659357039744b195fb1993c3ca69762701dc
OGRoomService <51579862+OGRoomService@users.noreply.github.com>
2021-11-09 14:40:10 -0500

    Preliminary login stuff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import { Route, Switch } from 'react-router';
+import { useState } from 'react';
+import { Route, Switch, useHistory } from 'react-router';
+
 import UserPage from './components/UserPage';
 import LoginPage from './components/LoginPage';
 import MainPage from './components/MainPage';
 import ProfilePage from './components/ProfilePage';
 import NotFound from './components/NotFound';
 import { Registration } from './components/Registration/Registration';
+
 import './App.css';
 
 export default function App() {
+  const [token, setToken] = useState();
+  const history = useHistory();
+
+  // If the token doesn't exist, only allow access to login and registration page
+  if (!token) {
+    return (
+      <Switch>
+        <Route path="/register">
+          <Registration />
+        </Route>
+        <Route exact path="/">
+          <LoginPage />
+        </Route>
+        <Route>
+          {history.push('/')}
+        </Route>
+      </Switch>
+    )
+  }
+
   return (
-    /*
-     * Do check to see if user logged in
-     * if they are show home page, if not show login page
-    */
     <Switch>
-      <Route exact path="/" component={LoginPage} />
-      <Route exact path="/home" component={MainPage} />
-      <Route exact path="/register" component={Registration} />
+      <Route exact path="/" component={MainPage} />
       <Route path="/u/:id" component={UserPage} />
       <Route exact path="/profile" component={ProfilePage} />
       <Route component={NotFound} />
