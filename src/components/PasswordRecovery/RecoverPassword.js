a018e5ca6c0ffc28a1efb1f175f86fd0661e853d
CharlesStanski <90924992+CharlesStanski@users.noreply.github.com>
2021-12-05 03:26:50 -0500

    Password Recovery mobile

--- src/components/PasswordRecovery/RecoverPassword.js
+++ src/components/PasswordRecovery/RecoverPassword.js
@@ -55,40 +55,53 @@ export const RecoverPassword = () => {
 
     const preSubmitForm = () => {
         return (
-            <Stack>
-                <Text>
-                    Reset Your Password
-                </Text>
-                <Text>Enter your email</Text>
-                <Input
-                    id="login-username"
-                    name="email"
-                    type="text"
-                    placeholder="Email"
-                    onChange={(e) =>
-                        setFormData({
-                            email: e.currentTarget.value
-                        })
-                    } />
+                // <Heading as="h2" size="4x5" mb="6">
+                //     <Text fontSize={{ base: "20px", sm: "20px", md: "20px", lg: "35px", xl: "80px" }} mt={{base: "32px", sm: "75px", md: "550px", lg:"55px"}}> Rowanspace </Text>
+                // </Heading>
+                <Stack
+                    w={{base:'350px', sm:'350px', md:'350px', lg:'45%'}}
+                    maxW={'600px'}
+                    mt={{base: "120px", sm: "120px", md: "120px", lg:"55px"}}
+                >
+                    <Text>
+                        Reset Your Password
+                    </Text>
+                    <Text>Enter your email</Text>
+                    <Input
+                        id="login-username"
+                        name="email"
+                        type="text"
+                        placeholder="Email"
+                        onChange={(e) =>
+                            setFormData({
+                                email: e.currentTarget.value
+                            })
+                        } />
 
-                {formErrors.email &&
-                    <Text className="error-message">{formErrors.email}</Text>
-                }
+                    {formErrors.email &&
+                        <Text className="error-message">{formErrors.email}</Text>
+                    }
 
-                <Input
-                    className='button'
-                    onClick={submitForm}
-                    type='button'
-                    value='Request' />
-            </Stack>
+                    <Input
+                        className='button'
+                        onClick={submitForm}
+                        type='button'
+                        value='Request' />
+                </Stack>
         );
     }
 
     const submittedForm = () => {
         return (
-            <Flex w="20em" h="100%" flexDirection={"column"} pos="fixed" alignItems="center" top="10%" left="38%">
-                <Heading>Request Sent!</Heading>
-                <Text>If an account with that email exists, you will receive an email shortly!</Text>
+            <Flex w="20em" h="100%" flexDirection={"column"} alignItems="center" top="10%" left="38%">
+                <Center 
+                    w={'100%'}
+                >
+                    <Stack mt={{base: "150px", sm:"150px"}}>
+                        <Heading>Request Sent!</Heading>
+                        <Text>If an account with that email exists, you will receive an email shortly!</Text>
+                    </Stack>
+                </Center>
             </Flex>
         );
     }
@@ -97,10 +110,11 @@ export const RecoverPassword = () => {
         <Flex
             flexDir={'column'}
             minH={'100vh'}
+            // mt={{base: "32px", sm: "750px", md: "350px", lg:"55px"}}
         >
             <Header />
             <Heading as="h2" size="4x5" mb="6">
-                <Text fontSize="6xl" mt="20"> Rowanspace </Text>
+                <Text fontSize={{ base: "20px", sm: "20px", md: "20px", lg: "35px", xl: "80px" }} mt={{base: "32px", sm: "75px", md: "550px", lg:"55px"}}> Rowanspace </Text>
             </Heading>
             <Center w={'100%'}>
                 {submitted ?
